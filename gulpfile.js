a309beb2a729836d0acd2d7aad9351ab06568fd7
Václav Uruba <uruba@outlook.com>
2016-03-14 19:54:49 +0100

    – initial commit

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require("gulp");
+var slim = require("gulp-slim");
+
+gulp.task('slim', function(){
+  gulp.src("./src/*.slim")
+    .pipe(slim({
+      pretty: true
+    }))
+    .pipe(gulp.dest("./dist/"));
+});
