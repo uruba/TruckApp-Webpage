b8505c4e80a540469c055bd8212a4668057d887e
Václav Uruba <uruba@outlook.com>
2016-03-23 00:14:24 +0100

    – initial website version

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,49 @@
 var gulp = require("gulp");
 var slim = require("gulp-slim");
+var stylus = require("gulp-stylus");
+var browserSync = require('browser-sync').create();
 
-gulp.task('slim', function(){
-  gulp.src("./src/*.slim")
-    .pipe(slim({
-      pretty: true
-    }))
-    .pipe(gulp.dest("./dist/"));
+var paths = {
+  dist: "dist/",
+  src: "src/"
+}
+
+// Static server
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: paths.dist
+      }
+  });
+  
+    gulp.watch(paths.dist.concat("**/*")).on('change', browserSync.reload);
 });
+
+gulp.task("slim", function(){
+gulp.src(paths.src.concat("slim/*.slim"))
+  .pipe(slim({
+    pretty: true
+  }))
+  .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task("stylus", function() {
+  gulp.src(paths.src.concat("stylus/main.styl"))
+  .pipe(stylus({
+    paths: ['node_modules'],
+    import: ['jeet/stylus/jeet', 'rupture/rupture', 'nib']
+  }))
+  .pipe(gulp.dest(paths.dist.concat("css/")));
+});
+
+gulp.task("images", function() {
+  gulp.src(paths.src.concat("img/*.{jpg, png}"))
+  .pipe(gulp.dest(paths.dist.concat("images/")));
+});
+
+gulp.task("watch", function() {
+  gulp.watch(paths.src.concat("slim/*.slim"), ["slim"]);
+  gulp.watch(paths.src.concat("stylus/*.styl"), ["stylus"]);
+});
+
+gulp.task("default", ["slim", "stylus", "browser-sync", "watch"]);
